da37b101d09de695d07f5c3e720632f1f3f62c58
sona2711 <sonamkrtchyan2711@gmail.com>
2024-06-19 13:56:58 +0400

    Added part of the structure

---
+++ main.js
@@ -0,0 +1,7 @@
+// f8d9b2288b40bfb3f9bcc617bee55f80 API
+function getWeather() {
+    const userInput = document.getElementById('user-input');
+    const cityName = userInput.value
+    userInput.value = ""
+    return  console.log(cityName)
+}
